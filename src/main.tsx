dc341ab0004a8ffbcdd166bc453da6ac155b185e
dhruvwarriorr <dhruvkumar31712@gmail.com>
2026-08-16 23:46:44 +0530

    font

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource-variable/manrope";
+import "@fontsource-variable/space-grotesk";
 import { FloatChatApp } from "./components/FloatChatApp";
 import "./globals.css";
 
